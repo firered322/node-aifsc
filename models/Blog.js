465f9c1dae2b72eb17998feb15fb6e3c58255da0
Atul <atulsingh1396@gmail.com>
2020-10-08 22:40:40 +0530

    reverted a few issues to previous working version

--- models/Blog.js
+++ models/Blog.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 
 const BlogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true, // trim any whitespace
-    },
-    body: {
-        type: String,
-        required: true,
-    },
-    // linking it up to a User model
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "users",
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  title: {
+    type: String,
+    required: true,
+    trim: true, // trim any whitespace
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Blog", BlogSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,26 @@ const multer = require("multer");
 
 const Blog = require("../models/Blog");
 const helpers = require("../helper");
-const authMiddleware = require("../middleware/auth");
+// const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
 // multer validation middleware
-const upload = multer({
-  dest: "files",
-  limits: {
-    fileSize: 2000000, // 2MB limit
-  },
-  //   to check if the uploaded document is a pdf
-  fileFilter(req, file, cb) {
-    // check file extension
-    //TODO: Save the file.originalname string to the database.
-    if (!file.originalname.endsWith(".pdf")) {
-      return cb(new Error("Please upload a PDF"));
-    }
-    cb(undefined, true);
-  },
-});
+// const upload = multer({
+//   dest: "files",
+//   limits: {
+//     fileSize: 2000000, // 2MB limit
+//   },
+//   //   to check if the uploaded document is a pdf
+//   fileFilter(req, file, cb) {
+//     // check file extension
+//     //TODO: Save the file.originalname string to the database.
+//     if (!file.originalname.endsWith(".pdf")) {
+//       return cb(new Error("Please upload a PDF"));
+//     }
+//     cb(undefined, true);
+//   },
+// });
 
 // @route  GET /
 // @desc   Homepage
@@ -49,7 +49,7 @@ router.get("/login", (req, res) => {
 // @route  GET /blog
 // @desc   Open the add blog page
 // @access Private
-router.get("/blog", authMiddleware, (req, res) => {
+router.get("/blog", (req, res) => {
   res.render("add_blog");
 });
 
@@ -65,14 +65,14 @@ router.get("/blog/:id", async (req, res) => {
 // @route  POST /blog
 // @desc   Create blog post from the blog form
 // @access Private
-router.post("/blog", authMiddleware, async (req, res) => {
+router.post("/blog", async (req, res) => {
   try {
     const { title, body } = req.body;
+    console.log(title, body);
     // const file = req.file;
     const blogObject = {
       title: title,
       body: body,
-      user: req.user.id,
     };
     // console.log(blogObject);
     await Blog.create(blogObject);
@@ -85,7 +85,7 @@ router.post("/blog", authMiddleware, async (req, res) => {
 // @route  POST /listblogs
 // @desc   List all the blogs
 // @access Private
-router.get("/listblogs", authMiddleware, async (req, res) => {
+router.get("/listblogs", async (req, res) => {
   try {
     const blogs = await Blog.find({}).sort({ id: -1 });
     res.render("list-blogs", { blogs });
@@ -97,7 +97,7 @@ router.get("/listblogs", authMiddleware, async (req, res) => {
 // @route  GET /edit-blog/:id
 // @desc   Open edit blog page
 // @access Private
-router.get("/edit-blog/:id", authMiddleware, async (req, res) => {
+router.get("/edit-blog/:id", async (req, res) => {
   const blog = await Blog.findOne({ _id: req.params.id }).lean();
   res.render("edit-blog", { blog });
 });
@@ -105,7 +105,7 @@ router.get("/edit-blog/:id", authMiddleware, async (req, res) => {
 // @route  POST /edit-blog/:id
 // @desc   Edit a blog
 // @access Private
-router.post("/edit-blog/:id", authMiddleware, async (req, res) => {
+router.post("/edit-blog/:id", async (req, res) => {
   const { title, body } = req.body;
   try {
     await Blog.findByIdAndUpdate({ _id: req.params.id }, { title, body });
